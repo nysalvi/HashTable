abed50eaf4f1df0d4615023e6cb80e4baa3e92fe
nysalvi <nysalvi20@gmail.com>
2025-05-20 19:20:02 -0300

    update: moved dense array initialization table.c->entry.c

---
+++ entry.c
@@ -0,0 +1,20 @@
+#include <stdint.h>
+#include <stdlib.h>
+#include "entry.h"
+
+void StartDenseArrayAsEmpty(EntryManager* manager) {
+	for (uint64_t i = 0; i < manager->length; i++) {
+		manager->entries[i] = NULL;
+	}
+}
+
+EntryManager* InitEntryManager(const uint64_t length) {
+	EntryManager* entryManager = malloc(sizeof(EntryManager) + sizeof(Entry*) * length);
+	entryManager->entries = entryManager + 1;
+	//table->entryManager->entries = malloc(sizeof(Entry) * length);
+	entryManager->length = length;
+	entryManager->usedLength = 0;
+	entryManager->global_insertion_counter = 0;
+	StartDenseArrayAsEmpty(entryManager);
+	return entryManager;
+}
