9fe0514ecb3c5ebccd6ff39a9719d942c5e7f51c
nysalvi <nysalvi20@gmail.com>
2025-05-20 19:22:13 -0300

    update: entry manager stores Entry** instead of Entry* to allow sorting

--- table.c
+++ table.c
@@ -4,63 +4,41 @@
 #include "sstring.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
-void StartSparseArrayAsUnused(Table* table) {
-	size_t pointerSize = table->indexManager->pSize;
-	IDX_TYPE empty = IDX_UNUSED;
-	for (uint64_t i = 0; i < table->indexManager->length; i += pointerSize) {
-		int8_t* p = &(table->indexManager->indices[i]);
-		memcpy(p, &empty, pointerSize);
-	}
-}
-void StartDenseArrayAsEmpty(Table* table) {
-	for (uint64_t i = 0; i < table->entryManager->length; i++) {
-		table->entryManager->entries[i].key.length = 0;
-	}
-}
-
-Table* CreateTable(const int length, const float loadFactor, const long (*hashFunction) (String idx)) {
-	int sizeIdx = GetSizeIdx(length);	
-	uint64_t indicesLength = length * 2;
-	size_t pointerSize = SIZE_POINTER[sizeIdx];
-
+Table* CreateTable(const uint64_t length, const float loadFactor, const long (*hashFunction) (String* idx)) {
 	Table* table = malloc(sizeof(Table));
-	table->indexManager = malloc(sizeof(SparseArrayManager) + pointerSize * indicesLength);
-	table->indexManager->indices = table->indexManager + 1;
-	table->indexManager->length = indicesLength;
-
 	if (!table)
 		return NULL;
-	//Table->indices = Table->indices + pointerSize;
-	table->removedEntriesIdx = NULL;
 
+	uint64_t indicesLength = length * 2;
+	table->indexManager = InitSparseArrayManager(indicesLength);
+	if (!(table->indexManager))
+		return NULL;
+	table->entryManager = InitEntryManager(length);
+	if (!(table->entryManager))
+		return NULL;
 
-	//Table* Table = malloc(sizeof(Table) + (sizeof(Entry) * (length - 1)));
-	table->entryManager = malloc(sizeof(EntryManager) + sizeof(Entry) * length);
-
-	table->entryManager->length = length;
-	table->entryManager->usedLength = 0;
+	table->removedEntriesIdx = InitIntLinkedList((int64_t) {0}, 0);
 	table->loadFactor = loadFactor;
 	table->hashFunction = hashFunction;
-	table->indexManager->length = indicesLength;
-	
-	table->indexManager->pSize = pointerSize;	
-	table->entryManager->entries = malloc(sizeof(Entry) * length);
-	StartSparseArrayAsUnused(table);
-	StartDenseArrayAsEmpty(table);
+		
 	return table;
 }
 
-void DestroyTable(Table* table, uint8_t removeEntries) {
-	if (removeEntries) {
-		uint64_t length = table->entryManager->length;
-		for (uint64_t i = 0; i < length; i++) {
-			free(table->entryManager->entries[i].value);
-		}
-	}
+void DestroyTable(Table* table, uint8_t deleteKeys, uint8_t deleteData) {
+	uint64_t length = table->entryManager->length;
+	for (uint64_t i = 0; i < length; i++) {
+		Entry* entry = table->entryManager->entries[i];
+		if (deleteKeys)
+			free(entry->key);
+		if (deleteData)
+			free(entry->value);
+		free(entry);
+	}	
 
-	free(table->entryManager);
 	free(table->indexManager);
+	free(table->entryManager);
 	free(table);
 }
 
@@ -71,16 +49,16 @@ Table* ResizeTable(Table* table) {
 	uint64_t newLength = oldLength * 2;
 
 	float loadFactor = table->loadFactor;
-	uint64_t(*hashFunction) (String idx) = table->hashFunction;
+	uint64_t(*hashFunction) (String* idx) = table->hashFunction;
 	
-	Entry* entries = table->entryManager->entries;
-	DestroyTable(table, 0);
+	Entry** entries = table->entryManager->entries;
+	DestroyTable(table, 0, 0);
 
 	Table* newTable = CreateTable(newLength, loadFactor, hashFunction);
 
 	for (int64_t i = 0; i < entriesUsed; i++) {
-		Entry entry = entries[i];
-		if (!entry.key.length)
+		Entry* entry = entries[i];
+		if (entry == NULL || !entry->key->length)
 			continue;
 
 	}
@@ -95,10 +73,10 @@ inline int8_t* GetSparseIndexPointer(const Table* table, uint64_t sparseIdx) {
 Entry* GetEntryFromSparseIndex(const Table* table, uint64_t sparseIndex) {
 	int8_t* pSparseIndex = GetSparseIndexPointer(table, sparseIndex);
 	int64_t entriesIdx = GetDenseIndexFromSparseIndexPointer(pSparseIndex, table->indexManager->pSize);
-	return &(table->entryManager->entries[entriesIdx]);
+	return table->entryManager->entries[entriesIdx];
 }
 inline Entry* GetEntryFromDenseIndex(const Table* table, const uint64_t denseIndex) {
-	return &(table->entryManager->entries[denseIndex]);
+	return table->entryManager->entries[denseIndex];
 }
 
 void* GetValueFromSparseIndex(const Table* table, uint64_t sparseIdx) {
@@ -106,7 +84,7 @@ void* GetValueFromSparseIndex(const Table* table, uint64_t sparseIdx) {
 	return entry->value;
 }
 
-uint64_t GetSparseIndexFromKey(Table* table, const String key) {
+uint64_t GetSparseIndexFromKey(Table* table, const String* key) {
 	const uint64_t mask = table->indexManager->length - 1;
 	const uint64_t hash = table->hashFunction(key);
 
@@ -121,7 +99,7 @@ uint64_t GetSparseIndexFromKey(Table* table, const String key) {
 			return i;
 		else {
 			Entry* entry = GetEntryFromDenseIndex(table, denseIdx);
-			if (!strcmp(entry->key.data, key.data))
+			if (!strcmp(entry->key->data, key->data))
 				return i;
 		}
 		perturb >>= PERTURB_SHIFT;
@@ -130,44 +108,60 @@ uint64_t GetSparseIndexFromKey(Table* table, const String key) {
 	printf("error: list is full and key is not in the list;");
 	exit(-1);	
 }
+/*
+Index* GetIndex(Table* table, String* key) {
+	uint64_t i = GetSparseIndexFromKey(table, key);
+	int8_t* p = GetSparseIndexPointer(table, i);
+	int64_t denseIdx = GetDenseIndexFromSparseIndexPointer(p, table->indexManager->pSize);
 
-
-void* GetValueFromKey(const Table* table, const String key) {
-	int64_t sparseIdx = GetSparseIndexFromKey(table, key);		
+	Index* index = malloc(sizeof(Index));
+	index->p = p;
+	index->pSize = table->indexManager->pSize;
+	index->denseIdx = denseIdx;
+	return index;
+}
+*/
+void* GetValueFromKey(const Table* table, const String* key) {
+	uint64_t sparseIdx = GetSparseIndexFromKey(table, key);		
 	void* data = GetValueFromSparseIndex(table, sparseIdx);
 	return data;
 }
 
 
-void InsertEntry(Table* table, String key, void* value) {
+void InsertEntry(Table* table, String* key, void* value) {
 	uint64_t sparseIdx = GetSparseIndexFromKey(table, key);
 	int8_t* p = GetSparseIndexPointer(table, sparseIdx);
 	
 	
-	Entry* entry;
-	uint64_t denseIdx;
+	Entry* entry = malloc(sizeof(Entry));
+	int64_t denseIdx;
 	if (table->removedEntriesIdx && table->removedEntriesIdx->size) {
 		denseIdx = PopIntNode(table->removedEntriesIdx);
-		entry = &(table->entryManager->entries[denseIdx]);
+		//entry = table->entryManager->entries[denseIdx];		
 	}
 	else {
 		denseIdx = table->entryManager->usedLength;
-		entry = &(table->entryManager->entries[denseIdx]);
+		//entry = table->entryManager->entries[denseIdx];		
 		table->entryManager->usedLength++;	
 	}
-
+	table->entryManager->entries[denseIdx] = entry;
 	memcpy(p, &denseIdx, table->indexManager->pSize);	
 	entry->insertCounter = table->entryManager->global_insertion_counter++;
 	entry->key = key;
 	entry->value = value;
 }
 
-void DeleteEntryTable(Table* table, String key, uint8_t deleteData) {
+void DeleteTableEntry(Table* table, String* key, uint8_t deleteData) {
 	uint64_t sparseIdx = GetSparseIndexFromKey(table, key);
 	int8_t* p = GetSparseIndexPointer(table, sparseIdx);
-	uint64_t denseIdx = GetDenseIndexFromSparseIndexPointer(p, table->indexManager->pSize);
-
-	Entry* entry = table->entryManager->entries + denseIdx;
+	int64_t denseIdx = GetDenseIndexFromSparseIndexPointer(p, table->indexManager->pSize);
+	Entry* entry = table->entryManager->entries[denseIdx];
 	if (deleteData)
-		free(entry);
+		free(entry->value);
+	free(entry);
+	table->entryManager->entries[denseIdx] = NULL;
+	
+	PushIntNode(table->removedEntriesIdx, denseIdx);	
+	IDX_TYPE unused = IDX_UNUSED;
+	memcpy(p, &unused, table->indexManager->pSize);
 }
