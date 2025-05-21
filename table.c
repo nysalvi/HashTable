471ceeb0060ab1da696e9929ffd81477224fd851
nysalvi <nysalvi20@gmail.com>
2025-05-21 17:52:37 -0300

    fix: deleting entries before inserting into the new table when resizing; fixed indices not being promoted to their size.

--- table.c
+++ table.c
@@ -5,6 +5,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdint.h>
+#include "quicksort.h"
+#include "entry.h"
 
 Table* CreateTable(const uint64_t length, const float loadFactor, const long (*hashFunction) (String* idx)) {
 	Table* table = malloc(sizeof(Table));
@@ -20,6 +22,9 @@ Table* CreateTable(const uint64_t length, const float loadFactor, const long (*h
 		return NULL;
 
 	table->removedEntriesIdx = InitIntLinkedList((int64_t) {0}, 0);
+	if (!(table->removedEntriesIdx))
+		return NULL;
+
 	table->loadFactor = loadFactor;
 	table->hashFunction = hashFunction;
 		
@@ -27,42 +32,21 @@ Table* CreateTable(const uint64_t length, const float loadFactor, const long (*h
 }
 
 void DestroyTable(Table* table, uint8_t deleteKeys, uint8_t deleteData) {
-	uint64_t length = table->entryManager->length;
-	for (uint64_t i = 0; i < length; i++) {
-		Entry* entry = table->entryManager->entries[i];
-		if (deleteKeys)
-			free(entry->key);
-		if (deleteData)
-			free(entry->value);
-		free(entry);
-	}	
-
+	uint64_t length = table->entryManager->length;	
+	free(table->removedEntriesIdx);	
 	free(table->indexManager);
+
+	if (deleteKeys || deleteData) {
+		for (uint64_t i = 0; i < length; i++) {
+			Entry* entry = table->entryManager->entries[i];
+			if (deleteKeys)
+				free(entry->key);
+			if (deleteData)
+				free(entry->value);
+		}
+	}
 	free(table->entryManager);
 	free(table);
-}
-
-
-Table* ResizeTable(Table* table) {
-	uint64_t entriesUsed = table->entryManager->usedLength;
-	uint64_t oldLength = table->entryManager->length;
-	uint64_t newLength = oldLength * 2;
-
-	float loadFactor = table->loadFactor;
-	uint64_t(*hashFunction) (String* idx) = table->hashFunction;
-	
-	Entry** entries = table->entryManager->entries;
-	DestroyTable(table, 0, 0);
-
-	Table* newTable = CreateTable(newLength, loadFactor, hashFunction);
-
-	for (int64_t i = 0; i < entriesUsed; i++) {
-		Entry* entry = entries[i];
-		if (entry == NULL || !entry->key->length)
-			continue;
-
-	}
-	return newTable;
 }
 
 
@@ -71,6 +55,7 @@ inline int8_t* GetSparseIndexPointer(const Table* table, uint64_t sparseIdx) {
 }
 
 Entry* GetEntryFromSparseIndex(const Table* table, uint64_t sparseIndex) {
+	
 	int8_t* pSparseIndex = GetSparseIndexPointer(table, sparseIndex);
 	int64_t entriesIdx = GetDenseIndexFromSparseIndexPointer(pSparseIndex, table->indexManager->pSize);
 	return table->entryManager->entries[entriesIdx];
@@ -128,30 +113,42 @@ void* GetValueFromKey(const Table* table, const String* key) {
 }
 
 
-void InsertEntry(Table* table, String* key, void* value) {
+void UpdateTable(Table* table, String* key, void* value) {
 	uint64_t sparseIdx = GetSparseIndexFromKey(table, key);
 	int8_t* p = GetSparseIndexPointer(table, sparseIdx);
-	
-	
-	Entry* entry = malloc(sizeof(Entry));
+		
 	int64_t denseIdx;
-	if (table->removedEntriesIdx && table->removedEntriesIdx->size) {
-		denseIdx = PopIntNode(table->removedEntriesIdx);
-		//entry = table->entryManager->entries[denseIdx];		
-	}
-	else {
-		denseIdx = table->entryManager->usedLength;
-		//entry = table->entryManager->entries[denseIdx];		
-		table->entryManager->usedLength++;	
+	if (table->removedEntriesIdx && table->removedEntriesIdx->size) 
+		denseIdx = PopIntNode(table->removedEntriesIdx);	
+	else 
+		denseIdx = table->entryManager->usedLength++;			
+	
+	Entry* entry = GetEntryFromDenseIndex(table, denseIdx);
+	if (entry == NULL) {
+		entry = malloc(sizeof(Entry));
+		entry->insertCounter = table->entryManager->global_insertion_counter++;
 	}
 	table->entryManager->entries[denseIdx] = entry;
-	memcpy(p, &denseIdx, table->indexManager->pSize);	
-	entry->insertCounter = table->entryManager->global_insertion_counter++;
+
+	void* dense;
+	int8_t dense8 = denseIdx;
+	int16_t dense16 = denseIdx;
+	int32_t dense32 = denseIdx;
+
+	if (table->indexManager->pSize == sizeof(int8_t))
+		dense = &dense8;
+	else if (table->indexManager->pSize == sizeof(int16_t))
+		dense = &dense16;
+	else if (table->indexManager->pSize == sizeof(int32_t))
+		dense = &dense32;
+	else
+		dense = &denseIdx;
+	memcpy(p, dense, table->indexManager->pSize);	
 	entry->key = key;
 	entry->value = value;
 }
 
-void DeleteTableEntry(Table* table, String* key, uint8_t deleteData) {
+void DeleteEntry(Table* table, String* key, uint8_t deleteData) {
 	uint64_t sparseIdx = GetSparseIndexFromKey(table, key);
 	int8_t* p = GetSparseIndexPointer(table, sparseIdx);
 	int64_t denseIdx = GetDenseIndexFromSparseIndexPointer(p, table->indexManager->pSize);
@@ -162,6 +159,69 @@ void DeleteTableEntry(Table* table, String* key, uint8_t deleteData) {
 	table->entryManager->entries[denseIdx] = NULL;
 	
 	PushIntNode(table->removedEntriesIdx, denseIdx);	
-	IDX_TYPE unused = IDX_UNUSED;
-	memcpy(p, &unused, table->indexManager->pSize);
+	
+	void* dense;
+	int8_t dense8 = IDX_UNUSED;
+	int16_t dense16 = IDX_UNUSED;
+	int32_t dense32 = IDX_UNUSED;
+	int64_t dense64 = IDX_UNUSED;
+
+	if (table->indexManager->pSize == sizeof(int8_t))
+		dense = &dense8;
+	else if (table->indexManager->pSize == sizeof(int16_t))
+		dense = &dense16;
+	else if (table->indexManager->pSize == sizeof(int32_t))
+		dense = &dense32;
+	else
+		dense = &dense64;
+	memcpy(p, dense, table->indexManager->pSize);
+}
+
+Table* ResizeTable(Table* table) {
+	float loadFactor = table->loadFactor;
+
+	size_t indexSize = table->indexManager->pSize;
+	free(table->indexManager);
+
+	uint64_t(*hashFunction) (String * idx) = table->hashFunction;	
+	uint64_t entriesUsed = table->entryManager->usedLength;
+
+	uint64_t newLength = table->entryManager->length * 2;
+	Entry** entries = table->entryManager->entries;
+	IntLinkedList* removedEntries = table->removedEntriesIdx;
+	
+	uint64_t i = entriesUsed - 1;
+	uint64_t entriesSize = entriesUsed - 1 - removedEntries->size;
+	while (removedEntries->size) {
+		Entry* entry = entries[i];
+		if (entry == NULL || !entry->key->length) {
+			i--;
+			continue;
+		}
+		int64_t denseIdx = PopIntNode(removedEntries);
+		entries[denseIdx] = entry;
+
+		entries[i] = NULL;
+		i--;
+	}
+	free(table->removedEntriesIdx);
+
+	QuickSortArgs* qSortArgs = malloc(sizeof(QuickSortArgs));
+	qSortArgs->data = entries;
+	qSortArgs->start = 0;
+	qSortArgs->end = entriesSize;
+	qSortArgs->ToInt = EntryToInt;
+
+	QuickSort(qSortArgs);
+	Table* newTable = CreateTable(newLength, loadFactor, hashFunction);
+
+	for (uint64_t j = 0; j < entriesSize; j++) {
+		Entry* entry = entries[j];
+		if (entry != NULL)
+			UpdateTable(newTable, entry->key, entry->value);
+		free(entry);
+	}
+	free(table->entryManager);
+	free(table);
+	return newTable;
 }
