99e3277a6b9c7925eec3c658568fca0f0eafa6e5
nysalvi <nysalvi20@gmail.com>
2025-05-21 17:46:52 -0300

    fix: String content now points to the correct memory address

--- sstring.c
+++ sstring.c
@@ -2,10 +2,11 @@
 #include <string.h>
 #include <stdlib.h>
 
-String* CreateString(char str[]) {
+String* CreateString(char str[]) {	
 	size_t length = strlen(str);
-	String* string = malloc(sizeof(String) + (length - 1));
-	string->length = length;
+	String* string = malloc(sizeof(String) + length + 1);
+	string->data = string + 1;
+	strcpy_s(string->data, length+1, str);
 	return string;
 }
 
