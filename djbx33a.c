b00b4cb8c48ff67575f8253bfd9f01cec39ff4ba
nysalvi <nysalvi20@gmail.com>
2025-05-21 17:47:50 -0300

    fix: hash incorrectly used signed int64

--- djbx33a.c
+++ djbx33a.c
@@ -2,12 +2,12 @@
 #include "djbx33a.h"
 #include "sstring.h"
 
-uint64_t DJBX33A(String key) {
-	int length = key.length;
+uint64_t DJBX33A(String* key) {
+	int length = key->length;
 	if (!length)
 		return 0;
-	long hash = 5381;
-	const uint8_t* p = &(key.data);
+	uint64_t hash = 5381;
+	const uint8_t* p = &(key->data);
 
 	if (length <= DJBX33A_LENGTH) {
 		for (int i = 0; i < length; i++) {
